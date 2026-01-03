92c490aafc8abe78740f6c13b7df5ba3224df557
Abdulrhman <abdulrhman@example.com>
2026-01-03 23:54:04 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: '/figma/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
